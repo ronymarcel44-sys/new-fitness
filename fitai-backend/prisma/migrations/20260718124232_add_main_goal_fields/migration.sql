9664d5da70f7820fcba9ca7cd41d58e0327bdd54
ronymarcel44-sys <ronymarcel44@gmail.com>
2026-07-18 16:07:55 +0300

    task 5 after edit

---
+++ fitai-backend/prisma/migrations/20260718124232_add_main_goal_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "mainTargetBenchPress" DOUBLE PRECISION,
+ADD COLUMN     "mainTargetBodyFatPct" DOUBLE PRECISION,
+ADD COLUMN     "mainTargetCardioDuration" INTEGER,
+ADD COLUMN     "mainTargetDeadlift" DOUBLE PRECISION,
+ADD COLUMN     "mainTargetSquat" DOUBLE PRECISION,
+ADD COLUMN     "mainTargetWeight" DOUBLE PRECISION;
